00ffae429111aa3d8c727abf177f1b0c6eb80e1f
Corten Singer <cortensinger@berkeley.edu>
2015-06-07 11:21:13 -0400

    Change in the jQuery function

--- function.js
+++ function.js
@@ -1,41 +1,41 @@
 var main = function() {
     $('.slider1').cycle({ 
-	fx:     'scrollHorz', 
+	fx:     'fade', 
 	speed:  'slow',
 	next:   '#next1', 
 	prev:   '#prev1' 
     });
     
     $('.slider2').cycle({ 
-	fx:     'scrollHorz', 
+	fx:     'fade', 
 	speed:  'slow',
 	next:   '#next2', 
 	prev:   '#prev2' 
     });
 
     $('.slider3').cycle({ 
-	fx:     'scrollHorz', 
+	fx:     'fade', 
 	speed:  'slow',
 	next:   '#next3', 
 	prev:   '#prev3' 
     });
 
     $('.slider4').cycle({ 
-	fx:     'scrollHorz', 
+	fx:     'fade', 
 	speed:  'slow',
 	next:   '#next4', 
 	prev:   '#prev4' 
     });
 
     $('.slider5').cycle({ 
-	fx:     'scrollHorz', 
+	fx:     'fade', 
 	speed:  'slow',
 	next:   '#next5', 
 	prev:   '#prev5' 
     });
 
     $('.slider6').cycle({ 
-	fx:     'scrollHorz', 
+	fx:     'fade', 
 	speed:  'slow',
 	next:   '#next6', 
 	prev:   '#prev6' 
