bf56a185c441135125127e9ed7ee1b621d5a3358
Corten Singer <cortensinger@berkeley.edu>
2015-06-18 20:24:16 -0400

    Image sliders messing with the timeout

--- function.js
+++ function.js
@@ -2,7 +2,7 @@ var main = function() {
     $('.slider1').cycle({ 
 	fx:     'fade', 
 	speed:  'slow',
-	delay: -200000,
+	delay: -100001,
 	timeout: 100000,
 	next:   '#next1', 
 	prev:   '#prev1' 
